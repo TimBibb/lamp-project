6d033709b1c4ccd382f82fe4c94628687cc66f2d
TimBibb <timothybibbee@gmail.com>
2021-08-27 18:33:02 -0400

    register in progress

--- js/code.js
+++ js/code.js
@@ -58,6 +58,62 @@ function doLogin()
 
 }
 
+function doRegister()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var login = document.getElementById("login").value;
+	var password = document.getElementById("loginPassword").value;
+	
+//	var hash = md5( password );
+	
+	document.getElementById("registerResult").innerHTML = "";
+
+	var tmp = {firstName:firstName,lastName:lastName,login:login,password:password};
+//	var tmp = {login:login,password:hash};
+	var jsonPayload = JSON.stringify( tmp );
+	
+	var url = urlBase + '/Register.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		// xhr.onreadystatechange = function() 
+		// {
+		// 	if (this.readyState == 4 && this.status == 200) 
+		// 	{
+		// 		var jsonObject = JSON.parse( xhr.responseText );
+		// 		userId = jsonObject.id;
+		
+		// 		if( userId < 1 )
+		// 		{		
+		// 			document.getElementById("registerResult").innerHTML = "User/Password combination incorrect";
+		// 			return;
+		// 		}
+		
+		// 		firstName = jsonObject.firstName;
+		// 		lastName = jsonObject.lastName;
+
+		// 		saveCookie();
+	
+		// 		window.location.href = "color.html";
+		// 	}
+		// };
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("registerResult").innerHTML = err.message;
+	}
+
+}
+
 function saveCookie()
 {
 	var minutes = 20;
