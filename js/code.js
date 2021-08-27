a1c374846e7556f56b7ed91dcc568af20eeb60ec
TimBibb <timothybibbee@gmail.com>
2021-08-27 19:10:49 -0400

    added redirect to login page

--- js/code.js
+++ js/code.js
@@ -82,9 +82,30 @@ function doRegister()
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		saveCookie();
-		window.location.href = "color.html";
+		// xhr.onreadystatechange = function() 
+		// {
+		// 	if (this.readyState == 4 && this.status == 200) 
+		// 	{
+		// 		var jsonObject = JSON.parse( xhr.responseText );
+		// 		userId = jsonObject.id;
+
+		// 		if( userId < 1 )
+		// 		{		
+		// 			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+		// 			return;
+		// 		}
+		
+		// 		firstName = jsonObject.firstName;
+		// 		lastName = jsonObject.lastName;
+
+		// 		saveCookie();
+	
+		// 		window.location.href = "color.html";
+		// 	}
+		// };
 		xhr.send(jsonPayload);
+		saveCookie();
+		window.location.href = "index.html"
 	}
 	catch(err)
 	{
