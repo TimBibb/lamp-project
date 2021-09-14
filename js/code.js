a47b500c16cda49f9f8b26bd560de57a8f5f90de
TimBibb <timothybibbee@gmail.com>
2021-09-13 20:47:06 -0400

    fix

--- js/code.js
+++ js/code.js
@@ -129,7 +129,9 @@ function saveCookie()
 	var minutes = 20;
 	var date = new Date();
 	date.setTime(date.getTime()+(minutes*60*1000));	
-	document.cookie = "firstName=" + firstName + ";lastName=" + lastName + ";userId=" + userId + ";expires=" + date.toGMTString();
+	document.cookie = "firstName=" + firstName + ";expires=" + date.toGMTString();
+	document.cookie = "lastName=" + lastName + ";expires=" + date.toGMTString();
+	document.cookie = "userId=" + userId + ";expires=" + date.toGMTString();
 }
 
 function readCookie()
